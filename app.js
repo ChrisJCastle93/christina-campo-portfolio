09a0346d643ac6c1ac4c3b40a69ea5e80a599ff7
Chris Castle <chrisjcastle93@gmail.com>
2022-02-06 11:40:26 +0100

    server set up

---
+++ app.js
@@ -0,0 +1,31 @@
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const hbs = require("hbs");
+const logger = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+
+const index = require("./routes/index");
+app.use("/", index);
+
+app.use(function (req, res, next) {
+  res.status(404).render("404");
+});
+
+app.listen(3000, () => {
+    console.log(`Listening on http://localhost:3000`);
+  });
+
+module.exports = app;
