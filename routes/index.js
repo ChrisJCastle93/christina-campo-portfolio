517cc9677c12c0c0fb14d9e183a52652f14e5b61
Chris Castle <chrisjcastle93@gmail.com>
2024-08-05 16:51:10 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router  = express.Router();
 
+/* Serve robots.txt */
+router.get('/robots.txt', (req, res) => {
+  res.type('text/plain');
+  res.send('User-agent: *\nDisallow: /');
+});
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
